0495b84582aacd6da8e2e4f6f2ae9488518e8b7c
MartinZ96 <martynasss16@gmail.com>
2021-05-03 17:53:45 +0300

    variables

---
+++ js/01-variables.js
@@ -0,0 +1,40 @@
+const amzius = 24;
+const pirstai = 5;
+const lapai = 2;
+
+const manoAmzius = 24;
+console.log(manoAmzius);
+
+const manoPirstai = 5;
+console.log(manoPirstai);
+
+const manoLapai = 2;
+console.log(manoLapai);
+
+const pauksciai = 'gaidys';
+console.log(pauksciai);
+
+const maistas = 'desra';
+console.log(maistas);
+
+const kelnes = 'juodos';
+console.log(kelnes);
+
+const pinigai = [5, 10, 20, 50, 100];
+console.log(pinigai);
+
+const sveikiPirstai = [1, 2, 3, 4, 5];
+console.log(sveikiPirstai);
+
+const saldainiu = [1, 2, 3, 4, 5];
+console.log(saldainiu);
+
+const spalvos = ['balta', 'juoda', 'melyna', 'zalia', 'geltona'];
+console.log(spalvos);
+
+const vardai = ['Matas', 'Jonas', 'Tomas', 'Lina', 'Greta'];
+console.log(vardai);
+
+const miestai = ['Vilnius', 'Palanga', 'Utena', 'Ignalina', 'Kaunas'];
+console.log(miestai);
+
